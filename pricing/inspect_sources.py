f48b5a270ad9b2d33829e0cf0ff5b70155023f7c
Leonardo Escobar <105086410+leoescobarh@users.noreply.github.com>
2026-09-10 12:05:05 -0300

    Inspeccionar recursos públicos de búsqueda de Unimarc

--- pricing/inspect_sources.py
+++ pricing/inspect_sources.py
@@ -28,7 +28,7 @@ def inspect_page(html):
                 walk(child, path + '/' + str(i))
     for i, node in enumerate(doc.nodes):
         attrs = node['attrs']
-        if node['tag'] == 'script' and attrs.get('src') and '/pages/' in attrs['src']:
+        if node['tag'] == 'script' and attrs.get('src'):
             result['assets'].append(attrs['src'])
         if node['tag'] == 'script' and (attrs.get('type') == 'application/ld+json' or attrs.get('id') == '__NEXT_DATA__'):
             try:
@@ -53,12 +53,13 @@ if __name__ == '__main__':
             try:
                 result = inspect_page(client.get(url))
                 print(json.dumps({'store': sid, 'url': url, 'result': result}, ensure_ascii=False)[:26000], flush=True)
-                for asset in result['assets']:
-                    if any(key in asset for key in ('/search-', '/_app-')):
+                for asset in result['assets'][:80]:
+                    if asset.startswith('/') or asset.startswith('https://www.unimarc.cl'):
                         code = client.get(urljoin(url, asset))
                         urls = sorted(set(re.findall(r'https://[a-zA-Z0-9./_-]+', code)))
-                        contexts = [code[max(0, m.start()-150):m.end()+230] for m in list(re.finditer(r'intelliSearch|productSearch|searchProducts|api\.unimarc|api/search', code))[:25]]
-                        print(json.dumps({'asset': asset, 'publicUrls': urls[:60], 'searchCode': contexts}, ensure_ascii=False), flush=True)
+                        contexts = [code[max(0, m.start()-180):m.end()+260] for m in list(re.finditer(r'intelliSearch|productSearch|searchProducts|api[./]|graphql|search\?', code, re.I))[:12]]
+                        if contexts or urls:
+                            print(json.dumps({'asset': asset, 'publicUrls': urls[:30], 'searchCode': contexts}, ensure_ascii=False), flush=True)
             except PriceError as error:
                 print(json.dumps({'store': sid, 'url': url, 'error': error.code, 'message': str(error)}, ensure_ascii=False), flush=True)
                 break
